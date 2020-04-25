f4b929a3590d3511b4b80be4eddc3f21ff7ea66d
Alvin <axu5604@bths.edu>
2020-04-25 19:37:32 -0400

    added bullet image

--- main.py
+++ main.py
@@ -26,8 +26,11 @@ playerX_change = 0
 enemyImg = pygame.image.load('enemy.png')
 enemyX = random.randint(0, 736)
 enemyY = random.randint(40, 125)
-enemyX_change = 0.25
+enemyX_change = 2
 enemyY_change = 35
+
+# Bullet Image
+bulletImg = pygame.image.load('bullet')
 
 
 def player(x, y):
@@ -72,10 +75,10 @@ while True:
 
     enemyX += enemyX_change
     if enemyX <= 0:
-        enemyX_change = 4
+        enemyX_change = 2
         enemyY += enemyY_change
     elif enemyX >= 736:
-        enemyX_change = -4
+        enemyX_change = -2
         enemyY += enemyY_change
 
     player(playerX, playerY)
